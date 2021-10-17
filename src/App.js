f93e85b63e6fec7ace9d62f49bb14c313367005a
Mitch <mitchellbrunke@gmail.com>
2021-10-17 19:46:24 +1000

    mobile userinfo done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState, useEffect, useRef } from "react";
+import Infobox from "./components/Infobox";
+
+//search by username
+//view info about user
 
 function App() {
+  //state for user
+  const [userData, setUserData] = useState(null);
+  //state for form input
+  const [formInput, setFormInput] = useState("");
+  //state to take input and set it as the userName
+  const [formData, setFormData] = useState("");
+  //useRef to get form input
+  const formRef = useRef();
+
+  //username fetch prep
+  const username = formData;
+
+  useEffect(() => {
+    //function to fetch data
+    const fetchData = async () => {
+      await fetch(`https://api.github.com/users/${username}`)
+        .then((response) => response.json())
+        .then((data) => setUserData(data))
+        .catch((err) => console.log(err));
+    };
+
+    //check if formData exists - saves hits on the API
+    if (formData) {
+      fetchData();
+    }
+
+    console.log("useEffect ran");
+
+    // cleanup
+    return () => {
+      setFormData("");
+    };
+  }, [formData, username]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="top">
+        <h2>devfinder</h2>
+        <p>light</p>
+      </div>
+
+      <form
+        onSubmit={(e) => {
+          //prevent re-render
+          e.preventDefault();
+          //set the username to whatever is entered in the form
+          setFormData(formInput);
+        }}
+      >
+        <input
+          type="text"
+          placeholder="search GitHub username..."
+          onChange={(e) => setFormInput(e.target.value)}
+          ref={formRef}
+        />
+        <button>Search</button>
+      </form>
+
+      {/* check if form data exists and render */}
+
+      {userData && <Infobox userData={userData} />}
     </div>
   );
 }
---
+++ src/components/Infobox.js
@@ -0,0 +1,73 @@
+import "../App.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faBuilding,
+  faCoffee,
+  faLink,
+  faMapMarkerAlt,
+  faMapPin,
+} from "@fortawesome/free-solid-svg-icons";
+
+const Infobox = ({ userData }) => {
+  //options for local date method
+  const options = { year: "numeric", month: "short", day: "numeric" };
+  //take the date data returned and turn into string
+  const date_string = userData.created_at.slice(0, 10);
+  //run date method on it
+  const output = new Date(date_string);
+  //convert to au local and in right format
+  const joinedDate = output.toLocaleDateString("en-AU", options);
+
+  return (
+    <div className="userContainer">
+      {console.log(userData)}
+      <div className="top">
+        <div className="avatar">
+          <img src={userData.avatar_url} alt="" />
+        </div>
+        <div className="userInfo">
+          <h3 id="name">{userData.name}</h3>
+          <h6 id="handle">@{userData.login}</h6>
+          <p id="date">Joined {joinedDate}</p>
+        </div>
+      </div>
+
+      <div className="bio">
+        {userData.bio ? <p>{userData.bio}</p> : <p>This profile has no bio</p>}
+      </div>
+
+      <div className="statbox">
+        <div className="info">
+          <h5>Repos</h5>
+          <p id="number">{userData.public_repos}</p>
+        </div>
+        <div className="info">
+          <h5>Followers</h5>
+          <p id="number">{userData.followers}</p>
+        </div>
+        <div className="info">
+          <h5>Following</h5>
+          <p id="number">{userData.following}</p>
+        </div>
+      </div>
+
+      <div className="randomInfo">
+        <p>
+          <FontAwesomeIcon class="icon" icon={faMapMarkerAlt} />
+          {userData.location}
+        </p>
+        <p>
+          {" "}
+          <FontAwesomeIcon class="icon" icon={faLink} />
+          {userData.blog}
+        </p>
+        <p>
+          <FontAwesomeIcon class="icon" icon={faBuilding} />
+          {userData.company}
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Infobox;
